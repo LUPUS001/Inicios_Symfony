85ed42c13d82aaa4e6cd39f0a541ce15433f0b70
LUPUS001 <antoniodaw23@gmail.com>
2026-03-16 13:55:06 +0100

    Commit 4.1: Metodos mágicos

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,31 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class PostController extends AbstractController
 {
-    #[Route('/post', name: 'app_post')]
-    public function index(): Response
+    // Recibir el id desde la URL
+    #[Route('/post/{id}', name: 'app_post')]
+    public function index(Post $post): Response
     {
+        dump($post);
+        
         return $this->render('post/index.html.twig', [
-            'quien_eres' => [
-                'antonio' => 'Hola soy Antonio',
-                'julio' => 'Hola soy Julio',
-                'aaron' => 'Hola soy Aarón',
-                'alexia' => 'Hola soy Alexia',
-                'ioel' => 'Hola soy Ioel',
-            ],
-            'edad' => 10110,
-            'numeros' => [[1,2,3,4,5],[1,2,3,4]],
-            'comidas' => [
-                'macarrones' => '12€',
-                'pizza' => '11€', 
-                'lasaña' => '10€',
-                'hamburguesa' => '8€',
-            ],
+            // Pasamos el post al twig, lo renderizamos para que lo pinte por pantalla
+            'post' => $post,
+
         ]);
     }
 }
